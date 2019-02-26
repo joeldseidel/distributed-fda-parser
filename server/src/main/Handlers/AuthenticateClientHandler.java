5e47c5942680559d3a7251be2667ae9d49f56d4f
Joel Seidel <jds6294@psu.edu>
2019-02-25 23:56:23 -0500

    Server handshake verification

--- server/src/main/Handlers/AuthenticateClientHandler.java
+++ server/src/main/Handlers/AuthenticateClientHandler.java
@@ -1,14 +1,28 @@
 package main.Handlers;
 
 import com.sun.net.httpserver.HttpHandler;
+import main.Managers.SessionInstanceManager;
 import org.json.JSONObject;
 
 public class AuthenticateClientHandler extends HandlerPrototype implements HttpHandler {
     public AuthenticateClientHandler(){
-        requiredKeys = new String[] { "session_id", "verification_key" };
+        requiredKeys = new String[] { "session_code", "verification_key" };
+        handlerName = "Authenticate User Handler";
     }
     @Override
     protected void fulfillRequest(JSONObject requestParams){
-
+        String sessionCode = requestParams.getString("session_code");
+        //Verify the session code
+        if(SessionInstanceManager.isValidSessionCode(sessionCode)){
+            //Session code was valid ~ attach the client as an authenticated worked
+            long verificationKey = requestParams.getLong("verification_key");
+            //Attach the client to as an authenticated worker
+            SessionInstanceManager.attachClient(verificationKey);
+            //Return the validated verification as a the response
+            this.response = Long.toString(verificationKey);
+        } else {
+            //Session code was invalid
+            this.response = "invalid client handshake";
+        }
     }
 }
