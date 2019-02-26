bebcedf70c119fe70257e9e8e51bfd8ed18c3c59
Joel Seidel <jds6294@psu.edu>
2019-02-25 23:36:16 -0500

    Generate session id and keys

--- server/src/main/Managers/SessionInstanceManager.java
+++ server/src/main/Managers/SessionInstanceManager.java
@@ -1,16 +1,35 @@
 package main.Managers;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SessionInstanceManager {
+    //This is the code entered into the client to register with this session
+    private static String sessionCode;
+    private static final int sessionCodeLength = 6;
+    //Numeric session key generated to verify the workers
     private static long sessionKey;
+    //List of the registered clients check sums
     private static long[] workerCheckSums;
+
+    /**
+     * Generate session code string
+     */
+    public static String generateSessionCode() {
+        //62 code character candidates
+        final String codeCandidates = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+        StringBuilder codeBuilder = new StringBuilder(sessionCodeLength);
+        for(int i = 0; i < 6; i++){
+            //Add a new random character from the candidates string
+            codeBuilder.append(codeCandidates.charAt(ThreadLocalRandom.current().nextInt(codeCandidates.length())));
+        }
+        sessionCode = codeBuilder.toString();
+        return sessionCode;
+    }
 
     /**
      * Set the unique ID of this session
      */
     public static void generateSessionKey() {
-        Random sessionKeyGen = new Random();
-        sessionKey = sessionKeyGen.nextLong();
+        sessionKey = ThreadLocalRandom.current().nextLong();
     }
 }
